afb1ccc883211828510b118c8cef08a90c2e8520
pablokreick <pablokreick@gmail.com>
2022-03-28 16:58:45 -0300

    desafio 4

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,9 +1,18 @@
+import ItemCount from '../ItemCount';
 import React from 'react';
 import Title from '../Title';
 
 export const ItemListContainer = ({ texto }) => {
+
+    const onAdd = (quantity) => {
+        console.log(`Compraste ${quantity} unidades`);
+    }
+
     return (
-        <Title greeting={texto} />
+        <>
+            <Title greeting={texto} />
+            <ItemCount initial={3} stock={5} onAdd={onAdd} />
+        </>
     );
 }
 
