8bf1a48ff7bdeee72492ec735fa818e3400cec5f
pablokreick <pablokreick@gmail.com>
2022-03-28 17:44:32 -0300

    desafio 5

---
+++ src/components/Item/index.jsx
@@ -0,0 +1,14 @@
+import './item.css';
+
+import React from 'react';
+
+const Item = ({ info }) => {
+    return (
+        <a href='' className="film">
+            <img src={info.image} alt="" />
+            <p>{info.title}</p>
+        </a>
+    );
+}
+
+export default Item;
